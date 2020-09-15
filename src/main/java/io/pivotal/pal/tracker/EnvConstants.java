1fa03fbe90d239ad2d9ca49e28dbf6f62554316d
Peter Banh <pbanh2@ford.com>
2020-09-15 17:00:16 +0000

    Final updates for lab "Building a Spring Boot App"

---
+++ src/main/java/io/pivotal/pal/tracker/EnvConstants.java
@@ -0,0 +1,10 @@
+package io.pivotal.pal.tracker;
+
+public class EnvConstants {
+
+    public static final String PORT = "PORT";
+    public static final String MEMORY_LIMIT = "MEMORY_LIMIT";
+    public static final String CF_INSTANCE_INDEX = "CF_INSTANCE_INDEX";
+    public static final String CF_INSTANCE_ADDR = "CF_INSTANCE_ADDR";
+
+}
--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,17 +1,43 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.pivotal.pal.tracker.EnvConstants;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class EnvController {
 
-    public EnvController() {
-        //
+    private final String port;
+    private final String memoryLimit;
+    private final String cfInstanceIndex;
+    private final String cfInstanceAddr;
+
+    public EnvController(
+            @Value("${port: NOT SET}") String port,
+            @Value("${memory.limit: NOT SET}") String memoryLimit,
+            @Value("${cf.instance.index: NOT SET}") String cfInstanceIndex,
+            @Value("${cf.instance.addr: NOT SET}") String cfInstanceAddr
+            ) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddr = cfInstanceAddr;
     }
 
     @GetMapping("/env")
-    public String getEnv() {
-        return null;
+    public Map<String, String> getEnv() {
+        Map<String, String> result = new HashMap<>();
+
+        result.put(EnvConstants.PORT, port);
+        result.put(EnvConstants.MEMORY_LIMIT, memoryLimit);
+        result.put(EnvConstants.CF_INSTANCE_INDEX, cfInstanceIndex);
+        result.put(EnvConstants.CF_INSTANCE_ADDR, cfInstanceAddr);
+
+        return result;
     }
 }
